f617c02d382aed0387359d2898a90dbfd869eb8e
Etni Alvis <etni.alvis@greenstonecs.com>
2020-08-16 21:27:45 -0400

    Initial with Binary Search Algorithms

---
+++ src/Algorithms/Algorithms/BinarySearch.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+
+        public Node(int value, Node left = default, Node right = default)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+
+    }
+
+
+    public static class BinarySearch
+    {
+        public static List<int> InOrder(Node node)
+        {
+
+            var list = new List<int>();
+
+            if (node == null)
+                return new List<int>();
+
+            var result = new List<int>();
+            result.AddRange(InOrder(node.Left));
+            result.Add(node.Value);
+            result.AddRange(InOrder(node.Right));
+
+
+            return result;
+        }
+
+        public static List<int> PreOrder(Node node)
+        {
+
+            var list = new List<int>();
+
+            if (node == null)
+                return new List<int>();
+
+            var result = new List<int>();
+            result.Add(node.Value);
+            result.AddRange(PreOrder(node.Left));
+            result.AddRange(PreOrder(node.Right));
+            return result;
+        }
+
+        public static Node Search(Node node, int value)
+        {
+            if (node == null || node.Value == value)
+                return node;
+
+            return value < node.Value ? Search(node.Left, value) : Search(node.Right, value);
+
+        }
+    }
+
+
+
+}
---
+++ src/Algorithms/Algorithms/Tests/BinarySearchTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class BinarySearchTests
+    {
+        //          4
+        //         / \
+        //        2   6
+        //       / \   \
+        //      1   3   7
+        public Node TestTree() => new Node(4, new Node(2, new Node(1), new Node(3)), new Node(6, null, new Node(7)));
+
+        [Fact]
+        public void ShouldTraverseInOrder()
+        {
+            var node = TestTree();
+
+            var result = BinarySearch.InOrder(node);
+
+            var expected = new List<int>() { 1, 2, 3, 4, 6, 7 };
+
+            Assert.Equal(expected, result);
+
+        }
+
+        [Fact]
+        public void ShouldTraversePreOrder()
+        {
+
+            var node = TestTree();
+
+            var result = BinarySearch.PreOrder(node);
+
+            var expected = new List<int>() { 4,2,1,3,6,7 };
+
+            Assert.Equal(expected, result);
+
+        }
+
+        [Fact]
+        public void ShouldFindAValue()
+        {
+            var node = TestTree();
+
+            var result = BinarySearch.Search(node, 6);
+
+            Assert.NotNull(result);
+            Assert.Equal(6, result.Value);
+        }
+
+        [Fact]
+        public void ShouldNotFindAValue()
+        {
+            var node = TestTree();
+
+            var result = BinarySearch.Search(node, 9);
+
+            Assert.Null(result);
+ 
+        }
+
+    }
+}
